3e307182ad4b32822e28e2a0c39d5a3408e9c5cf
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-12-23 16:40:37 -0800

    components for hero banner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -3,9 +3,20 @@ import { Box, Stack, Typography } from '@mui/material'
 
 const HeroBanner = () => {
   return (
-    <Box>
-        <Typography>
+    <Box sx={{
+        mt: { lg: '212px', xs: '70px'},
+        ml: {sm: '50px'}
+    }} position='relative' p='20px'>
+        <Typography color='#FF2625' fontWeight="600" fontSize='26px'>
             Fitness Club
+        </Typography>
+
+        <Typography>
+            Sweat, Smile <br/> and Repeat
+        </Typography>
+
+        <Typography>
+            SCheck out the most effective exercises
         </Typography>
     </Box>
   )
