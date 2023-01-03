cff75325f42556d7efe35b31336588d55d165661
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-01-02 18:49:50 -0800

    moving state to home component to pass down

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,18 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Box } from '@mui/material';
 import Exercises from '../components/Exercises';
 import SearchExercises from '../components/SearchExercises';
 import HeroBanner from '../components/HeroBanner';
 
 const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all')
+
   return (
     <Box>
         <HeroBanner />
-        <SearchExercises />
+        <SearchExercises setExercises={setExercises}
+        bodyPart={bodyPart} setBodyPart={setBodyPart}/>
         <Exercises />
     </Box>
   )
