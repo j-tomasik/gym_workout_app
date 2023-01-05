f79f163968163a02a53afadf48871bf7136a9f3c
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-01-04 19:09:49 -0800

    adding in right and left arrow

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import BodyPart from './BodyPart';
+import {ScrollMenu, VisibilityConext } from 'react-horizontal-scrolling-menu';
+
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollPrev()} className="right-arrow">
+      <img src={LeftArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollNext()} className="left-arrow">
+      <img src={RightArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
 
 const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
   return (
-    <div>
+    <ScrollMenu>
         {data.map((item) => 
         <Box
         key={item.id || item} 
@@ -14,7 +35,8 @@ const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
         >
             <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
         </Box>)}
-        </div>
+    </ScrollMenu>
+
   )
 }
 
