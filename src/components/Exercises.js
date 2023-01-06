9dbf675e1ca907bfa37493de5d58b072560f11d6
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-01-05 18:22:14 -0800

    setting up exercises component

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import Pagination  from "@mui/material/Pagination";
+import { Box, Stack, Typography } from '@mui/material';
 
-const Exercises = () => {
+import {exerciseOptions, fetchData } from '../util/fetchData';
+
+
+const Exercises = ({exercises, setExerercises, bodyPart}) => {
   return (
     <div>Exercises</div>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,8 +13,9 @@ const Home = () => {
         <HeroBanner />
         <SearchExercises setExercises={setExercises}
         bodyPart={bodyPart} setBodyPart={setBodyPart}/>
-        <Exercises setExercises={setExercises}
-        bodyPart={bodyPart} setBodyPart={setBodyPart}/>
+
+        <Exercises exercises={exercises} setExercises={setExercises}
+        bodyPart={bodyPart} />
     </Box>
   )
 }
