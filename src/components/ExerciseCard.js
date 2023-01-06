4804909f05884b96320db95daeb75a56943a5983
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-01-05 18:42:23 -0800

    adding buttons to exercise card

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -11,7 +11,15 @@ const ExerciseCard = ({ exercise}) => {
             fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize'}}>
                 {exercise.bodyPart}
             </Button>
+
+            <Button sx={{ml: '21px', color: '#fff', background: '#fcc757',
+            fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize'}}>
+                {exercise.targetMuscle}
+            </Button>
         </Stack>
+        <Typography ml='21px' color='#000' fontWeight='bold' mt='11px' pb='10px' textTransform='capitalize' fontSize='22px'> 
+            {exercise.name}
+        </Typography>
     </Link>
   )
 }
