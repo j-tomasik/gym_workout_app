60c8d1d47e7ab428fdaa5936fd611f8182fed0a2
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-01-10 15:18:39 -0800

    creating sub components for exercise detail page

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -5,7 +5,11 @@ import { exerciseOptions, fetchData } from '../utils/fetchData'
 
 const ExerciseDetail = () => {
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+      <Detail />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
   )
 }
 
