4a425190f02477923bcd304dada3c96785cb1a94
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-01-11 18:11:16 -0800

    adding sub components for each ex detail

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -8,6 +8,21 @@ import EquipmentImage from '../assets/icons/equipment.png';
 const Detail = ({ exerciseDetail }) => {
     const {bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
 
+    const extraDetail = [
+      {
+        icon: BodyPartImage,
+        name: bodyPart,
+      },
+      {
+        icon:TargetImage,
+        name: target,
+      },
+      {
+        icon: EquipmentImage,
+        name: equipment,
+      },
+    ]
+
   return (
     <Stack gap='60px' sx={{flexDirection: { lg: 'row'}, p: '20px', alignItems: 'center'}}>
         <img src={gifUrl} alt={name} loading='lazy' className='detail-image' />
@@ -17,11 +32,22 @@ const Detail = ({ exerciseDetail }) => {
             {name}
             </Typography>
             
-            <Typography variant='h5'>
+            <Typography variant='h6'>
                 Exercises keep you strong. {name} {` `}
                 is one of the best exercises to target you {target}. It will help you
                 improve your mood and gain energy.
             </Typography>
+            {extra.Detail.map((item) => {
+              <Stack key={item.name} direction='row' gap='24px' alignItems='center'>
+                <Button sx={{ background: '#fff2db', borderRadius: '50%', width: '100px', height: '100px'}}>
+                  <img src={item.icon} />
+                </Button>
+
+                <Typography variant='h5'>
+                  {item.name}
+                </Typography>
+              </Stack>
+            })}
         </Stack>
     </Stack>
   )
