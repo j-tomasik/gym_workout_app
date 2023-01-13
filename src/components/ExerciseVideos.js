129ac6de5f303cc9c22919958ba13c9c00009858
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-01-12 20:33:35 -0800

    setting up mui components for exercise videos

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {Box, Stack, Typography } from '@mui/material'
 
-const ExerciseVideos = () => {
+const ExerciseVideos = ({ExerciseVideos, name }) => {
   return (
-    <div>ExerciseVideos</div>
+    <Box sx={{ marginTop: {lg: '200px', xs: '20px'}}} p='20px'>
+      <Typography variant='h3' mb='33px'>
+        Watch <span style={{color: '#ff2625', textTransform: 'capitalize'}} >{name}</span> exercise videos
+      </Typography>
+
+      <Stack justifyContent='flex-start' flexWrap='wrap' alignItems='center'
+      sx={{flexDirection: {lg: 'row'},
+      gap: { lg: '110px', xs: '0'}
+      }}
+      >
+
+      </Stack>
+    </Box>
   )
 }
 
