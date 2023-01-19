ce858cb7a500ccce91d15c3a722dde70a2e59b56
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-01-19 11:23:57 -0800

    trying to fix nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,15 +7,15 @@ import Logo from '../assets/images/Logo.png'
 const Navbar = () => {
   return (
     <Stack direction='row' justify='space-around' sx={{ gap: {
-        sm: '122px', xs: '40px'}, mt: { sm: '32px', xs: '20px'}, justify: 'none' }} px='20px'>
+        sm: '123px', xs: '40px'}, mt: { sm: '32px', xs: '20px'}, justify: 'none' }} px='20px'>
         <Link to='/'>
             <img src={Logo} alt='logo' style={{
-                width: '48px', height: '48px', margin: '0 20px'
+                width: '48px', height: '48px', margin: '0px 20px'
             }} />
         </Link>
 
         
-            <Stack direction='row' gap='40px' fontSize='24px' alignItems='flex-end'>
+            <Stack direction='row' gap='40px' fontSize='24px' fontFamily="Alegreya" alignItems='flex-end'>
                 <Link to='/' style={{textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625'}}>Home</Link>
                 <a href='#exercises' style={{texteDecoration: 'none', color: '#3A1212'}}>Exercises</a>
             </Stack>
