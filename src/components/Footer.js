db4c5766ef6ecf9e67747a99ccd342b5e795ea01
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-01-19 11:19:37 -0800

    finished up footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,11 @@ const Footer = () => {
     return (
     <Box mt='80px' bgcolor='#fff3f4'>
         <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
+            <img src={Logo} alt='logo' width='200px' height='40px' mt='20px'/>
 
+            <Typography variant='h5' pb='40px' mt='20px'>
+                Made with ❤️ by Jack Tomasik
+            </Typography>
         </Stack>
     </Box>
     )
