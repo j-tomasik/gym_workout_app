2ff492ff4492f296016f976169736f9ba70338aa
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-01-19 12:21:20 -0800

    youtube api fixed

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import {Box, Stack, Typography } from '@mui/material'
+import {Box, Stack, Typography } from '@mui/material';
+import Loader from './Loader';
 
-const ExerciseVideos = ({ExerciseVideos, name }) => {
+const ExerciseVideos = ({exerciseVideos, name }) => {
 
-  if(!ExerciseVideos.length) return 'Loading...'
+  if(!exerciseVideos.length) return <Loader />;
+  
   return (
     <Box sx={{ marginTop: {lg: '200px', xs: '20px'}}} p='20px'>
       <Typography variant='h3' mb='33px'>
@@ -15,7 +17,7 @@ const ExerciseVideos = ({ExerciseVideos, name }) => {
       gap: { lg: '110px', xs: '0'}
       }}
       >
-        {ExerciseVideos?.slice(0, 3).map((item, index) => (
+        {exerciseVideos?.slice(0, 3).map((item, index) => (
           <a
           key={index}
           className='exercise-video'
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,6 +1,5 @@
 export const exerciseOptions = {
   method: 'GET',
-  url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
@@ -17,6 +16,8 @@ export const fetchData = async (url, options) => {
 
 export const youtubeOptions = {
   method: 'GET',
+  url: 'https://youtube-search-and-download.p.rapidapi.com/channel/about',
+  params: {id: 'UCE_M8A5yxnLfW0KghEeajjw'},
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
