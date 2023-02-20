b67baf768fb240a014551b7e52229b0776df139a
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-02-20 12:58:32 -0800

    replaced depricated spinner with better option

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Stack } from '@mui/material';
+import { Stack, CircularProgress } from '@mui/material';
 import { InfinitySpin } from 'react-loader-spinner';
 
 const Loader = () => {
   return (
     <Stack direction='row' justify='center' alignItems='center' width='100%'>
-        <InfinitySpin color='grey' />
+        <CircularProgress />
     </Stack>
   )
 }
