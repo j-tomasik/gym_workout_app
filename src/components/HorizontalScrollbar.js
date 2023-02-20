5249c46fe401cb6be1f52e6ffb674fdb755ece60
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-02-20 14:19:02 -0800

    added in the disable for arrows

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Button } from '@mui/material';
 import BodyPart from './BodyPart';
 import ExerciseCard from './ExerciseCard'
 import {ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
@@ -7,20 +7,20 @@ import RightArrowIcon from '../assets/icons/right-arrow.png';
 import LeftArrowIcon from '../assets/icons/left-arrow.png';
 
 const LeftArrow = () => {
-  const { scrollPrev } = useContext(VisibilityContext);
+  const { scrollPrev, isFirstItemVisible } = useContext(VisibilityContext);
 
   return (
-    <Typography onClick={() => scrollPrev()} className="right-arrow">
+    <Typography disabled={isFirstItemVisible} onClick={() => scrollPrev()} className="right-arrow">
       <img src={LeftArrowIcon} alt="right-arrow" />
     </Typography>
   );
 };
 
 const RightArrow = () => {
-  const { scrollNext } = useContext(VisibilityContext);
+  const { scrollNext, isLastItemVisible } = useContext(VisibilityContext);
 
   return (
-    <Typography onClick={() => scrollNext()} className="left-arrow">
+    <Typography disabled={isLastItemVisible} onClick={() => scrollNext()} className="left-arrow">
       <img src={RightArrowIcon} alt="right-arrow" />
     </Typography>
   );
